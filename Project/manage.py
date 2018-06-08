2399a3aa4889112b2b6f8ae7cee65a818aaab54b
firefist9 <Progress198@gmail.com>
2018-06-08 13:00:22 +0545

    hs

--- Project/manage.py
+++ Project/manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-#hello hello hello alkdsjf
+#hello hello hello alkdsjf fuck it all
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project.settings")
     try:
