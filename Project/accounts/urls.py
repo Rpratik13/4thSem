8eeb22e2e6fce2ac2baae187eba0fe8163b637c6
sukubhattu <sukubhattusandesh8@gmail.com>
2018-07-01 12:48:07 +0545

    account log out pani xa

--- Project/accounts/urls.py
+++ Project/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
 	path('login/', views.login, name='login'),
 	path('sign-up/', views.sign_up, name='sign-up'),
 	path('log-in/', views.log_in, name='log-in'),
+	path('log-out/', views.log_out, name='log-out'),
 ]
